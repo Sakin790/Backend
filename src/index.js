316d07ee294ab5917167d09d325441c29bc5a845
sakin islam <sakin islam79@gmail.com>
2024-01-01 10:01:27 +0600

    express connection done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import dotenv from "dotenv";
 import ConnectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-ConnectDB();
-
-
-
-
-
-
-
+ConnectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server in running at PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connecion Error!!!", "from root index file", err);
+  });
 
 /*
 import { express } from "express";
