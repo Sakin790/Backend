c4459b964eea2f8d401219275f46ad32fe35d05b
sakin islam <sakin islam79@gmail.com>
2024-01-03 15:29:22 +0600

    multer and cloudinary configuration

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -5,14 +5,14 @@ class apiError extends Error {
     errors = [],
     statck = ""
   ) {
-    super(message), 
-    (this.statusCode = statusCode);
+    super(message), (this.statusCode = statusCode);
     this.data = null;
+    this.message = message;
     this.success = false;
     this.errors = errors;
 
-    if (statck) {
-      this.stack = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
