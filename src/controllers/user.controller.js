e6e0c35d1ee8184578d365a11fbe124268cf03b5
sakin islam <sakin islam79@gmail.com>
2024-01-05 12:45:20 +0600

    updated router file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,68 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  const { fullName, email, username, password } = req.body;
+  //console.log("Here is email", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "all fields are required");
+  }
+
+  const existedUser = User.findOne({
+    //database a username and email exitst kore kina
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new apiError(409, "username or email already exist");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar is reqired");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new apiError(400, "Avatar is required");
+  }
+
+  const user = User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage.url || " ",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  const createUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createUser) {
+    throw new apiError(500, "Something went Wrong while registering the user");
+  }
+  return res
+    .status(201)
+    .json(new apiResponse(200, createUser, "user registred successfully"));
 });
 
 export { registerUser };
+
+//get user details from frontend => use postman
+//validation=> not empty
+//check if user allready exitst => username or emails
+//check for images and avatar
+//uplode them cloudinary
+//create user object => create entry in db
+//remove password and refresh token form response
+//check for user creation
+//return
